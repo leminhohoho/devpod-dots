79dae7bf91c66a6ed76bd369f04ad07b6778b19b
leminhohoho <leminhnguyenai@gmail.com>
2025-11-09 18:08:44 +0700

    Modify installing packages commands

--- setup.sh
+++ setup.sh
@@ -4,16 +4,8 @@ export XDG_CONFIG_HOME="$HOME"/.config
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 
-packages=(
-    fd
-    ripgrep
-    npm
-    lazygit
-)
-
-for package in "${packages[@]}"; do
-    echo "Installing $package ..."
-done
+go install github.com/jesseduffield/lazygit@latest
+npm install -g treesitter-cli
 
 echo "All packages are installed"
 
